40e5fd5c75a473fdd4e4c881db08e93ac0bcb3d1
Haywhy217 <olanipekunayomide217@gmail.com>
2024-08-17 10:10:55 +0100

    Added whitenoise

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -147,3 +147,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
